cf56dcbbdf968e4a2561e9a80233f63dc3c0867d
Amir4ik <amir4ik11@gmail.com>
2025-07-03 01:01:10 +0400

    2) Added random images to request

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,11 +1,13 @@
 package otus.homework.coroutines
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class CatsPresenter(
     private val catsService: CatsService,
+    private val imageService: ImageService,
     private val presenterScope: CoroutineScope,
     private val showToast: (String) -> Unit
 ) {
@@ -14,15 +16,26 @@ class CatsPresenter(
 
     fun onInitComplete() {
         presenterScope.launch {
-            try {
-                val fact = catsService.getCatFact()
-                _catsView?.populate(fact)
-            } catch (e: java.net.SocketTimeoutException) {
-                showToast.invoke("Не удалось получить ответ от сервером")
-            } catch (e: Exception) {
-                CrashMonitor.trackWarning()
-                showToast.invoke(e.message.toString())
-            }
+            val getFact  = async { processGet { catsService.getCatFact() } }
+            val getImage = async { processGet { imageService.getImage() }?.firstOrNull() }
+
+            val fact = getFact.await() ?: return@launch
+            val image = getImage.await() ?: return@launch
+
+            _catsView?.populate(Model(fact = fact, imageUrl = image.url))
+        }
+    }
+
+    private suspend fun <T> processGet(getData: suspend () -> T): T? {
+        return try {
+            getData.invoke()
+        } catch (e: java.net.SocketTimeoutException) {
+            showToast.invoke("Не удалось получить ответ от сервера")
+            null
+        } catch (e: Exception) {
+            CrashMonitor.trackWarning()
+            showToast.invoke(e.message.toString())
+            null
         }
     }
 
